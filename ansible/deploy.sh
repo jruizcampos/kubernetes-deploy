201f4b86f91a33eb464633912340e4749491415c
John Ruiz Campos <john.ruiz.campos@gmail.com>
2021-07-04 11:16:01 -0500

    v1.2

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-ansible -i hosts -m ping all
 
+#ansible -i hosts -m ping all
+
+# Ejecutamos las configuraciones comunes a todos los nodos
+ansible -i hosts 01-configuraciones-comunes.yml
+# Ejecutamos las configuraciones correspondientes a los nodos Master
+ansible -i hosts 02-configurando-master.yml
+# Ejecutamos las configuraciones correspondientes a los nodos NFS
+ansible -i hosts 03-configurando-nfs.yml
+# Ejecutamos las configuraciones correspondientes a los nodos Worker
+ansible -i hosts 04-configurando-workers.yml
