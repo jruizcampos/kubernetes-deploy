6231230f07133e0ac926b32aba76cfb4b47127f6
John Ruiz Campos <john.ruiz.campos@gmail.com>
2021-07-09 16:08:08 -0500

    v1.4.7

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -4,9 +4,11 @@
 
 # Ejecutamos las configuraciones comunes a todos los nodos
 ansible-playbook -i hosts 01-configuraciones-comunes.yml
+# Ejecutamos las configuraciones comunes a los masters y workers
+ansible-playbook -i hosts 02-configuraciones-master-worker.yml
 # Ejecutamos las configuraciones correspondientes a los nodos Master
-ansible-playbook -i hosts 02-configurando-master.yml
+ansible-playbook -i hosts 03-configurando-master.yml
 # Ejecutamos las configuraciones correspondientes a los nodos NFS
-ansible-playbook -i hosts 03-configurando-nfs.yml
+ansible-playbook -i hosts 04-configurando-nfs.yml
 # Ejecutamos las configuraciones correspondientes a los nodos Worker
-ansible-playbook -i hosts 04-configurando-workers.yml
+ansible-playbook -i hosts 05-configurando-workers.yml
