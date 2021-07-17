79b3dd83dfba9d7f3f90b9feeede1d32dbd64138
John Ruiz Campos <john.ruiz.campos@gmail.com>
2021-07-17 06:49:49 -0500

    v1.4.23

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,14 +5,5 @@
 # Ejecutamos el playbook de despliegue de Kubernetes
 ansible-playbook -i hosts despliegue-kubernetes.yml
 
-
-# Ejecutamos las configuraciones comunes a todos los nodos
-#ansible-playbook -i hosts 01-configuraciones-comunes.yml
-# Ejecutamos las configuraciones comunes a los masters y workers
-#ansible-playbook -i hosts 02-configuraciones-master-worker.yml
-# Ejecutamos las configuraciones correspondientes a los nodos Master
-#ansible-playbook -i hosts 03-configurando-master.yml
-# Ejecutamos las configuraciones correspondientes a los nodos NFS
-#ansible-playbook -i hosts 04-configurando-nfs.yml
-# Ejecutamos las configuraciones correspondientes a los nodos Worker
-#ansible-playbook -i hosts 05-configurando-workers.yml
+# Ejecutamos el playbook de despliegue de la aplicación
+ansible-playbook -i hosts despliegue-aplicacion.yml
