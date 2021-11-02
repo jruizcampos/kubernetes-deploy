75ff2ae10653b9b6978e2d64ddbbc66c20da9392
John Ruiz Campos <john.ruiz.campos@gmail.com>
2021-11-02 13:29:33 -0500

    v1.6.2

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -4,4 +4,4 @@
 ansible-playbook -i hosts despliegue-kubernetes.yml
 
 # Ejecutamos el playbook de despliegue de la aplicación
-#ansible-playbook -i hosts despliegue-aplicacion.yml
+ansible-playbook -i hosts despliegue-aplicacion.yml
